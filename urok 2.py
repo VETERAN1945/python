87c52370aa5f3136bad29fc2f6d921470f0b8e77
VETERAN1945 <max234189@gmail.com>
2023-07-30 15:48:52 +0300

    24124241

---
+++ urok 2.py
@@ -0,0 +1,30 @@
+a = 10
+b = 3
+
+# Addition
+addition_result = a + b
+print("Addition:", addition_result)
+
+# Subtraction
+subtraction_result = a - b
+print("Subtraction:", subtraction_result)
+
+# Division
+division_result = a / b
+print("Division:", division_result)
+
+# Multiplication
+multiplication_result = a * b
+print("Multiplication:", multiplication_result)
+
+# Exponent (Power)
+exponent_result = a ** b
+print("Exponent:", exponent_result)
+
+# Modulus
+modulus_result = a % b
+print("Modulus:", modulus_result)
+
+# Floor division
+floor_division_result = a // b
+print("Floor Division:", floor_division_result)
